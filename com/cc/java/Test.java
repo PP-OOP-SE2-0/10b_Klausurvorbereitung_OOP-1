e81cd16208ac7f4bcdc377fd860b0d60f5721b90
Sabine.Eitel <Seit201@aol.com>
2022-12-01 10:38:32 +0100

    klausur

--- com/cc/java/Test.java
+++ com/cc/java/Test.java
@@ -3,12 +3,14 @@ package com.cc.java;
 public class Test {
     
  void showAddress(){
-    System.out.println("Instanzvariable: " + this);
+    System.out.println("Instanzvariable: " + this);  
 }
 
-    	// this --> Instanzvariable
+    	// this --> Instanzvariable //gebunden an ein Object(nie Klasse)
 		// verweist aus der Instanz auf sich selbst
    
 
 
 }
+// instanzvariable + Reverenzvariable verweisen beide auf die gleiche Adresse,
+// kann von innen wie von außen angezeigt werden
